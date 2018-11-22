fc077b1b5948b27c63feeb5a69ba93de8c7b1ab0
Sutalite <32411868+Sutalite@users.noreply.github.com>
2018-11-22 16:59:32 +0100

    fixed top bar and underlined navbar tittle

---
+++ script.js
@@ -0,0 +1,44 @@
+const navbarElements = document.getElementById("navbar").getElementsByTagName("li");
+
+const categoriesStart = [
+    document.getElementById("Introduction"),
+    document.getElementById("LeSon"),
+];
+
+const categoriesEnd = [
+    document.getElementById("EndIntroduction"),
+    document.getElementById("EndLeSon"),
+];
+
+var activeIndex = 0;
+
+window.onscroll = function () {
+    checkNewVisible();
+}
+
+function checkNewVisible() {
+    for (let i = 0; i < categoriesStart.length; i++) {
+        if (i != activeIndex) {
+            if (checkVisible(categoriesStart[i]) || checkVisible(categoriesEnd[i])) {
+                SetNavbarElementStyle(navbarElements[i], "underline");
+                SetNavbarElementStyle(navbarElements[activeIndex], "none");
+                activeIndex = i;
+            } else {
+                SetNavbarElementStyle(navbarElements[i], "none");
+            }
+        }
+    }
+}
+
+function checkVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+function SetNavbarElementStyle(element, style) {
+    const link = element.getElementsByTagName("a")[0];
+    link.style.textDecoration = style;
+}
+SetNavbarElementStyle(navbarElements[0], "underline");
+checkNewVisible();
